a205d8e0b31ff0bf87dc21f8528361d5f9b3b93b
Amin <pahlavanmohammadamin@gmail.com>
2024-02-19 11:29:11 +0330

    game done

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ document.addEventListener('DOMContentLoaded', () =>{
   const grid = document.querySelector('.grid')
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50;
-  let doodlerBottomSpace = 250;
+  let startPoint = 150;
+
+  let doodlerBottomSpace = startPoint;
   let isGameOver = false;
   let platformCount = 5;
   let platforms = [];
+  let upTimerId
+  let downTimerId 
+  let isJumping = true;
+  let isGoingLeft = false;
+  let isGoingRight = false;
+  let leftTimerId 
+  let rightTimerId
+  let score = 0;
 
   function createDoodler () {
     grid.appendChild(doodler)
     doodler.classList.add('doodler')
+    doodlerLeftSpace = platforms[0].left;
     doodler.style.left = doodlerLeftSpace + 'px';
     doodler.style.bottom = doodlerBottomSpace + 'px'
   }
@@ -46,15 +57,126 @@ document.addEventListener('DOMContentLoaded', () =>{
         platform.bottom -= 4;
         let visual = platform.visual;
         visual.style.bottom = platform.bottom + 'px';
+
+        if(platform.bottom < 10) {
+          let firsPlatform = platforms[0].visual;
+          firsPlatform.classList.remove('platform')
+          platforms.shift();
+          score++;
+          let newPlatform = new Platform(600);
+          platforms.push(newPlatform)
+        }
       })
     }
   }
 
+  function jump() {
+    clearInterval(downTimerId);
+    isJumping = true;
+    upTimerId = setInterval(function () {
+      doodlerBottomSpace +=20;
+      doodler.style.bottom = doodlerBottomSpace + 'px';
+      if(doodlerBottomSpace > startPoint + 200) {
+        fall();
+      }
+    },30)
+  }
+
+  function fall() {
+    clearInterval(upTimerId);
+    isJumping = false;
+    downTimerId = setInterval(() => {
+      doodlerBottomSpace -=5;
+      doodler.style.bottom = doodlerBottomSpace + 'px';
+      if(doodlerBottomSpace <= 0){
+        doodlerBottomSpace
+        gameOver()
+      }
+      platforms.forEach(platform => {
+        if(
+          (doodlerBottomSpace>= platform.bottom) &&
+          (doodlerBottomSpace <= platform.bottom + 15) && 
+          ((doodlerLeftSpace + 60) >= platform.left) && 
+          (doodlerLeftSpace <= (platform.left + 85)) &&
+          !isJumping
+        ) {
+          console.log('landed')
+          startPoint = doodlerBottomSpace;
+          jump()
+        }
+      })
+    },30);
+  }
+
+  function gameOver() {
+    console.log('game over')
+    isGameOver = true;
+    while(grid.firstChild) {
+      grid.removeChild(grid.firstChild)
+    }
+    grid.innerHTML = score;
+    clearInterval(upTimerId)
+    clearInterval(downTimerId)
+    clearInterval(leftTimerId);
+    clearInterval(rightTimerId);
+  }
+
+  function control(e) {
+    if(e.key === 'ArrowLeft') {
+      moveLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveRight();
+    } else if (e.key=== 'ArrowUp') {
+      moveStraight();
+    }
+  }
+
+  function moveLeft() {
+    if(isGoingRight) {
+      clearInterval(rightTimerId)
+      isGoingRight = false;
+    }
+    isGoingLeft = true;
+    leftTimerId = setInterval(() =>{
+      if(doodlerLeftSpace >=0) {
+        doodlerLeftSpace -= 5;
+				doodler.style.left = doodlerLeftSpace + "px";
+      } else moveRight();
+
+   
+    },30)
+  }
+
+  function moveRight() {
+    if(isGoingLeft) {
+      clearInterval(leftTimerId);
+      isGoingLeft = false;
+    }
+    isGoingRight = true;
+    rightTimerId = setInterval(() =>{
+      if(doodlerLeftSpace <=340) {
+        doodlerLeftSpace +=5;
+        doodler.style.left = doodlerLeftSpace + 'px';
+      } else moveLeft()
+    },30)
+  }
+
+  function moveStraight () {
+    isGoingLeft = false;
+    isGoingLeft = false;
+    clearInterval(rightTimerId);
+    clearInterval(leftTimerId);
+
+  }
+
+
   function start() {
     if(isGameOver == false){
-      createDoodler();
       createPlatforms();
+      createDoodler();
       setInterval(movePlatforms,30);
+      jump()
+      document.addEventListener('keyup',control)
       
     }
   }
